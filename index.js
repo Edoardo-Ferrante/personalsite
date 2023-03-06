cc48421fdec454f49a77a3779391a90c7757eed6
Edoardo-Ferrante <eferrante@tylostudios.com>
2023-03-06 08:52:04 +0000

    Update

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+
+const browserSync = require('browser-sync').create();
+
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -13,5 +22,10 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Website is live at http://localhost:${port}`);
+
+    //browserSync.init({
+        //files: ['public/**/*.ejs', 'public/**/*.css'],
+        //proxy: 'http://localhost:3000'
+    //});
 });
 
